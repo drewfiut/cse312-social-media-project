becfd14d7579bc616833aa3463491eb762e83eca
Drew Fiutko <drewfiut@buffalo.edu>
2020-03-11 13:38:13 -0400

    Custom 404 Page

--- app.py
+++ app.py
@@ -49,5 +49,9 @@ def rocket_proj():
 def likes():
     return render_template('likes.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

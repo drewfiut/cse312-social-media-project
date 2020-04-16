b39dcff0b7054509b836a30c49a81b5d7c6e65d4
Drew Fiutko <drewfiut@buffalo.edu>
2020-04-16 18:17:03 -0400

    change port to 8000

--- app.py
+++ app.py
@@ -173,4 +173,4 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0')
+    socketio.run(app, debug=True, host='0.0.0.0', port=8000)
